46d6c4b722f396b3aa802e1b70ec0ecbe1e3d0de
cjreysayago <rey933@gmail.com>
2020-01-29 15:57:04 -0300

    First commit

---
+++ src/LaravelGoogleJobsServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Lightit\LaravelGoogleJobs;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelGoogleJobsServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
